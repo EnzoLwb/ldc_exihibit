c94eeea99e284ad8834efdecbc0b132884d1a05f
zhoujianye <111@123.com>
2018-04-27 09:36:55 +0800

    修改样式i

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -16,10 +16,10 @@
 @section('body')
     <div style="height: 100%;  width: 100%; background: url(../img/bg.jpg) center center;    background-size: 100% 100%;    min-width: 1200px;    overflow: hidden;">
     <div class="middle-box text-center loginscreen" style=" position: relative; top: 40%; transform: translateY(-50%);">
-        <div id="loginBox" style=" width:300px; height: auto; margin: 0 auto;">
+        <div id="loginBox" style=" width:395px; height: auto; margin: 0 auto;">
             <div style=" margin-bottom: 20px; font-size: 35px; color: #fff; text-align: center;">{{$system_name}}</div>
 
-            <form class="m-t" role="form" method="POST" action="{{ route('admin.login') }}" style=" width: 250px; margin: 0 auto;">
+            <form class="m-t" role="form" method="POST" action="{{ route('admin.login') }}" style=" width: 280px; margin: 0 auto;">
                 {{ csrf_field() }}
                 <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
                     <input type="text" name="username" class="form-control" placeholder="请输入用户名" required="" value="{{ old('username') }}"/>
